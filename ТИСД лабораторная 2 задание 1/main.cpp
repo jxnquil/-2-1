ff0b19316f667115de4b20de3275d3257d7e1d2f
jxnquil <V.sabantcev@mail.ru>
2024-02-21 20:41:58 +0700

    Initial Commit

---
+++ ТИСД лабораторная 2 задание 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ТИСД лабораторная 2 задание 1
+//
+//  Created by Vladimir Sabantcev on 21.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
